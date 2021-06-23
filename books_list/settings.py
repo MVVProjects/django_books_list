ae0ff0af680a30f8a4708585a049712ea0f09096
MVVProjects <mich.walentynowicz@gmail.com>
2021-06-23 04:34:21 +0200

    Added files and changes for deployment

--- books_list/settings.py
+++ books_list/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     
     # 3rd party
     'rest_framework',
-    'import_export',
     
     # Default django apps
     'django.contrib.admin',
@@ -53,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,6 +129,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+# The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
 
 # Default primary key field type
@@ -150,3 +156,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination', 
     'PAGE_SIZE': 10,
 }
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
